66a6dc9afac2432de6bdb87fb00dc341405252ec
Heidi Negrete <heidi@negrete.dev>
2025-01-29 13:41:15 -0500

    implement mvvm in viewmodels

--- AcademicAssistant.Core/ViewModels/HomeViewModel.cs
+++ AcademicAssistant.Core/ViewModels/HomeViewModel.cs
@@ -1,7 +1,35 @@
+using System.Collections.ObjectModel;
+using AcademicAssistant.Repositories;
+using AcademicAssistant.Repositories.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace AcademicAssistant.Core.ViewModels;
 
-public class HomeViewModel
+public class HomeViewModel: ObservableRecipient
 {
+    private readonly IRepository _repository;
+    
+    private Student _student;
+    public Student Student
+    {
+        get => _student;
+        set
+        {
+            SetProperty(ref _student, value);
+        }
+    }
     public string Title { get; set; } = "Academic Assistant";
-    public string StudentName { get; set; } = "John Doe";
+    
+    public ObservableCollection<Term> Terms { get; set; } = new();
+    
+    public HomeViewModel(IRepository repository)
+    {
+        _repository = repository;
+        Student = _repository.GetStudent();;
+    }
+
+    public void LoadStudent()
+    {
+       Student = _repository.GetStudent(); 
+    }
 }
--- AcademicAssistant.Core/ViewModels/SettingsViewModel.cs
+++ AcademicAssistant.Core/ViewModels/SettingsViewModel.cs
@@ -1,8 +1,40 @@
-using System;
+using AcademicAssistant.Repositories;
+using AcademicAssistant.Repositories.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AcademicAssistant.Core.ViewModels;
 
-public class SettingsViewModel
+public partial class SettingsViewModel : ObservableRecipient
 {
+    private readonly IRepository _repository;
+    
+    private Student _student;
+    public Student Student
+    {
+        get => _student;
+        set
+        {
+            SetProperty(ref _student, value);
+        }
+    }
 
+    public SettingsViewModel(IRepository repository)
+    {
+        _repository = repository;
+        Student = repository.GetStudent();
+    }
+    
+    public void LoadStudent()
+    {
+        Student = _repository.GetStudent(); 
+    }
+    
+    [RelayCommand]
+    public async void UpdateStudent()
+    {
+        _repository.UpdateStudent(_student.Id, _student);
+
+        await Shell.Current.GoToAsync($"//home");
+    }
 }
