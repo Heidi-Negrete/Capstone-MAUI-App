71599fbe9ce4f226695fee2bd1a97d48729a2cf2
Heidi Negrete <heidi@negrete.dev>
2025-01-29 13:40:22 -0500

    fix routing error by removing duplicate route from AppShell

--- AcademicAssistant/AppShell.xaml.cs
+++ AcademicAssistant/AppShell.xaml.cs
@@ -7,9 +7,7 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
-
-        Routing.RegisterRoute("home", typeof(HomePage));
-        Routing.RegisterRoute("settings", typeof(SettingsPage));
+        
         Routing.RegisterRoute("term", typeof(TermDetailsPage));
         Routing.RegisterRoute("course", typeof(CourseDetailsPage));
         Routing.RegisterRoute("assessment", typeof(AssessmentDetailsPage));
