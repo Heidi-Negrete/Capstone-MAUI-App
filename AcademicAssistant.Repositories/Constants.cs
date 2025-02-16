2fe5e9608835cdbffb6c81c1cc5ba119ca72adf6
Heidi Negrete <heidi@negrete.dev>
2025-02-16 18:52:15 -0500

    add sqlite-net-pcl package & configure constants

---
+++ AcademicAssistant.Repositories/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace AcademicAssistant.Repositories;
+
+public class Constants
+{
+    public const string DatabaseFilename = "AcademicAssistant.db3";
+    
+    public const SQLite.SQLiteOpenFlags Flags =
+        SQLite.SQLiteOpenFlags.ReadWrite |
+        SQLite.SQLiteOpenFlags.Create |
+        SQLite.SQLiteOpenFlags.SharedCache;
+    
+    //public static string DatabasePath => Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+    public static string DatabasePath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DatabaseFilename);
+}
