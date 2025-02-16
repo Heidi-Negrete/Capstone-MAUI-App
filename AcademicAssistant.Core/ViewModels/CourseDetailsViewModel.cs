70eda47809bc73d1bc58e4e286c4d8e03ee8572c
Heidi Negrete <heidi@negrete.dev>
2025-02-16 18:55:16 -0500

    implement course details page

--- AcademicAssistant.Core/ViewModels/CourseDetailsViewModel.cs
+++ AcademicAssistant.Core/ViewModels/CourseDetailsViewModel.cs
@@ -1,10 +1,50 @@
+using System.Collections.ObjectModel;
+using AcademicAssistant.Repositories;
+using AcademicAssistant.Repositories.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 namespace AcademicAssistant.Core.ViewModels;
 
-public class CourseDetailsViewModel
+public partial class CourseDetailsViewModel : ObservableRecipient
 {
-
-    public void LoadData(int courseId)
+    private readonly IRepository _repository;
+    
+    
+    [ObservableProperty] private Course _course;
+    
+    [ObservableProperty] private bool _assessmentSelected;
+    
+    [ObservableProperty] private Assessment _selectedAssessment;
+    
+    [ObservableProperty] private ObservableCollection<Assessment> _assessments;
+    
+    public CourseDetailsViewModel(IRepository repository)
     {
-        // Load data from database
+        _repository = repository;
+    }
+
+    public async void LoadData(int CourseId)
+    {
+        Course = await _repository.GetCourseById(CourseId);
+        Assessments = new ObservableCollection<Assessment>(await _repository.GetAssessmentsByCourseId(CourseId));
+        AssessmentSelected = false;
+    }
+    
+    [RelayCommand]
+    public void SelectionChanged(Assessment? assessment)
+    {
+        if (assessment == null) AssessmentSelected = false;
+        else
+        {
+            AssessmentSelected = true;
+        }
+    }
+
+    [RelayCommand]
+    public async void ViewSelectedAssessment()
+    {
+        if (SelectedAssessment == null) return;
+        await Shell.Current.GoToAsync($"assessment?id={SelectedAssessment.Id}");
     }
 }
