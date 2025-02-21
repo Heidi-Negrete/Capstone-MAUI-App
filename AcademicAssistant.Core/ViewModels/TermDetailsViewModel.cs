7ad92ef09e272cdc4ee3e140656aa05e4e1c9136
Heidi Negrete <heidi@negrete.dev>
2025-02-21 18:48:05 -0500

    validate term details page changes

--- AcademicAssistant.Core/ViewModels/TermDetailsViewModel.cs
+++ AcademicAssistant.Core/ViewModels/TermDetailsViewModel.cs
@@ -13,23 +13,27 @@ public partial class TermDetailsViewModel : ObservableRecipient
     
     
     [ObservableProperty] private Term _term;
-    
-    [ObservableProperty] private bool _courseSelected;
+
+    [ObservableProperty] private bool _courseSelected = false;
     
     [ObservableProperty] private Course _selectedCourse;
     
     [ObservableProperty] private ObservableCollection<Course> _courses;
+
+    [ObservableProperty] private bool _changesValid = true; // Used to indicate whether 'Save' button should be enabled
+
+    [ObservableProperty] private bool _termDataChanged = false; // Used to indicate whether 'Save' button should be shown
     
     public TermDetailsViewModel(IRepository repository)
     {
         _repository = repository;
-        CourseSelected = false;
     }
 
     public async void LoadData(int TermId)
     {
         Term = await _repository.GetTermById(TermId);
         Courses = new ObservableCollection<Course>(await _repository.GetCoursesByTermId(TermId));
+        TermDataChanged = false;
     }
     
     [RelayCommand]
@@ -70,5 +74,60 @@ public partial class TermDetailsViewModel : ObservableRecipient
     {
         if (SelectedCourse == null) return;
         await Shell.Current.GoToAsync($"course?id={SelectedCourse.Id}");
+    }
+
+    [RelayCommand]
+    public async void SaveChanges()
+    {
+        if (!ChangesValid)
+        {
+            await Shell.Current.DisplayAlert("Error", "Please check all fields before saving.", "OK");
+            return;
+        }
+        try
+        {
+            await _repository.UpdateTerm(Term.Id, Term);
+        }
+        catch (Exception e)
+        {
+            await Shell.Current.DisplayAlert("Failed to save changes.", e.Message, "OK");
+        }
+        TermDataChanged = false;
+    }
+    
+    [RelayCommand]
+    public async void AskUserToSaveChanges()
+    {
+        if (!TermDataChanged) await Shell.Current.GoToAsync("//home");
+        else
+        {
+            
+            if (ChangesValid)
+            {
+                var result = await Shell.Current.DisplayAlert("Save Changes", "You have unsaved changes. Would you like to save them?", "Yes", "No");
+                if (result)
+                {
+                    SaveChanges();
+                }
+            }
+            await Shell.Current.GoToAsync("//home");
+        }
+    }
+
+    [RelayCommand]
+    public async void ConfirmTermDataChanged()
+    {
+        TermDataChanged = true;
+        await ValidateData();
+    }
+    
+    private async Task ValidateData()
+    {
+        if (Term.StartDate > Term.EndDate)
+        {
+            ChangesValid = false;
+            await Shell.Current.DisplayAlert("Invalid Dates", "Please ensure your start date is before your end date.", "OK");
+            return;
+        }
     }
 }
