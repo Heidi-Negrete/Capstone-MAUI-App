49c31a92d81d08c0a70eae263d5fef68eb793b5c
Heidi Negrete <heidi@negrete.dev>
2025-02-21 20:20:29 -0500

    validate assessment detail page

--- AcademicAssistant.Core/ViewModels/AssessmentDetailsViewModel.cs
+++ AcademicAssistant.Core/ViewModels/AssessmentDetailsViewModel.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using AcademicAssistant.Repositories;
 using AcademicAssistant.Repositories.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AcademicAssistant.Core.ViewModels;
 
@@ -16,6 +17,9 @@ public partial class AssessmentDetailsViewModel : ObservableRecipient
 
     [ObservableProperty] private List<AcademicStatus.Status> _assessmentStatusList;
     
+    [ObservableProperty] private bool _dataChanged = false; // Used to indicate whether 'Save' button should be shown
+    [ObservableProperty] private bool _changesValid = true; // Used to indicate whether 'Save' button should be enabled
+    
     public AssessmentDetailsViewModel(IRepository repository)
     {
         _repository = repository;
@@ -25,5 +29,61 @@ public partial class AssessmentDetailsViewModel : ObservableRecipient
     {
         Assessment = await _repository.GetAssessmentById(AssessmentId);
         AssessmentStatusList = Enum.GetValues(typeof(AcademicStatus.Status)).Cast<AcademicStatus.Status>().ToList();
+        DataChanged = false;
+    }
+    
+    [RelayCommand]
+    public async void SaveChanges()
+    {
+        if (!ChangesValid)
+        {
+            await Shell.Current.DisplayAlert("Error", "Please check all fields before saving.", "OK");
+            return;
+        }
+        try
+        {
+            await _repository.UpdateAssessment(Assessment.Id, Assessment);
+        }
+        catch (Exception e)
+        {
+            await Shell.Current.DisplayAlert("Failed to save changes.", e.Message, "OK");
+        }
+        DataChanged = false;
+    }
+    
+    [RelayCommand]
+    public async void AskUserToSaveChanges()
+    {
+        if (!DataChanged) await Shell.Current.GoToAsync($"course?id={Assessment.CourseId}");
+        else
+        {
+            
+            if (ChangesValid)
+            {
+                var result = await Shell.Current.DisplayAlert("Save Changes", "You have unsaved changes. Would you like to save them?", "Yes", "No");
+                if (result)
+                {
+                    SaveChanges();
+                }
+            }
+            await Shell.Current.GoToAsync($"course?id={Assessment.CourseId}");
+        }
+    }
+
+    [RelayCommand]
+    public async Task ConfirmDataChanged()
+    {
+        DataChanged = true;
+        await ValidateData();
+    }
+    
+    private async Task ValidateData()
+    {
+        if (Assessment.StartDate > Assessment.EndDate)
+        {
+            ChangesValid = false;
+            await Shell.Current.DisplayAlert("Invalid Dates", "Please ensure your start date is before your end date.", "OK");
+            return;
+        }
     }
 }
